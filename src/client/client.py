069ae65e0030ca5dbe0f6719e015ccdecd1bfa4c
enatsu <unsioer@outlook.com>
2020-04-10 23:25:13 +0800

    Folder Transport

--- src/client/client.py
+++ src/client/client.py
@@ -1,43 +1,80 @@
 import os
 import sys
 from socket import *
+from pathlib import Path
 
 BUFSIZ = 1024
 
 
-def Send(cliSock):
-    filename = input('> ')
-    if os.path.exists(filename):
-        cliSock.send(bytes(filename, 'utf-8'))
-
-        filesize = str(os.path.getsize(filename))
-        print("文件大小为：", filesize)
-        cliSock.send(filesize.encode())
-        data = cliSock.recv(BUFSIZ)  # 挂起发送，确保伺服端单独收到文件大小数据，避免粘包
-        print("开始发送")
-        
-        f = open(filename, "rb")
-        for line in f:
-            cliSock.send(line)   
+def send(cliSock, filename, src_path):
+    filePath = Path(filename)
+    rel_path = ''
+    if(filePath.parent.name == ''):
+        rel_path = filename
     else:
-        print("Given filename not found!")
+        rel_path = str(filePath.parent.relative_to(
+            src_path).joinpath(filePath.name))
+    cliSock.send(bytes(rel_path, 'utf-8'))
+    data = cliSock.recv(BUFSIZ)
+    filesize = str(os.path.getsize(filename))
+    print("文件大小为：", filesize)
+    cliSock.send(filesize.encode())
+    data = cliSock.recv(BUFSIZ)  # 挂起发送，确保伺服端单独收到文件大小数据，避免粘包
+    print("开始发送")
+
+    f = open(filename, "rb")
+    for line in f:
+        cliSock.send(line)
+    data = cliSock.recv(BUFSIZ)
+
+
+'''
+    else:
+        print("Given folder path of client not found!")
         cliSock.send("\0\01".encode())
         return -1
+'''
+
+
+def getFileList(folder_path):
+    if os.path.isfile(folder_path):  # 文件
+        return [folder_path]
+    fileList = []
+    rootdir = folder_path
+    list = os.listdir(rootdir)  # 列出文件夹下所有目录与文件
+
+    for i in range(0, len(list)):
+        com_path = os.path.join(rootdir, list[i])
+        if os.path.isfile(com_path):
+            fileList.append(com_path)
+        if os.path.isdir(com_path):
+            fileList.extend(getFileList(com_path))
+    return fileList
 
 
 def usage():
     print("Usage: ")
-    print("  python client.py <path> <ip_addr> <port>")
+    print("  python client.py <src_path> <ip_addr> <port>")
 
 
 if __name__ == '__main__':
     if(len(sys.argv) < 4):
         usage()
-    else:
-        filename = sys.argv[1]
-        ip_addr = sys.argv[2]
-        port = int(sys.argv[3])
-        cliSock = socket(AF_INET, SOCK_STREAM)
-        cliSock.connect((ip_addr, port))
-        Send(cliSock)
-        cliSock.close()
+        sys.exit(1)
+
+    src_path = sys.argv[1]
+    ip_addr = sys.argv[2]
+    port = int(sys.argv[3])
+
+    if os.path.exists(src_path) == False:
+       print("Given folder error!")
+       sys.exit(1)
+    relativePath = getFileList(src_path)
+
+    cliSock = socket(AF_INET, SOCK_STREAM)
+    cliSock.connect((ip_addr, port))
+
+    for filename in relativePath:
+        send(cliSock, filename, src_path)
+    cliSock.send("\01".encode())
+    cliSock.close()
--- src/server/server.py
+++ src/server/server.py
@@ -1,44 +1,54 @@
 import os
 import sys
 from socket import *
-from time import ctime
+from pathlib import Path
+#from time import ctime
 
 BUFSIZ = 1024
 
-def recv(cliSock):
-    data = cliSock.recv(BUFSIZ)  # 接收文件名或错误信息
-    if data.decode() == "\0\01":
-        print("Sorry client's file not found")
-        return -1
-    
-    filename = data.decode("utf-8")
-    print("Received Filename:", filename)
 
-    data = cliSock.recv(BUFSIZ)
-    cliSock.send("File size received".encode())
+def recv(cliSock, folder_path):
+    while True:
+        data = cliSock.recv(BUFSIZ)  # 接收文件名或错误信息
+        if data.decode() == "\01":
+            print("End")
+            return 0
 
-    file_total_size = int(data.decode())
-    received_size = 0
-    f = open(filename, "wb")
-    while received_size < file_total_size:
+        filePath = data.decode("utf-8")
+        print("Received File Path: "+filePath)
+        cliSock.send("Get path".encode())
+
         data = cliSock.recv(BUFSIZ)
-        f.write(data)
-        received_size += len(data)
-        print("已接收：", received_size)
-    f.close()
+        cliSock.send("File size received ".encode())
+
+        savePath = Path(folder_path).joinpath(filePath)
+        Path(savePath.parent).mkdir(parents=True, exist_ok=True)
+
+        file_total_size = int(data.decode())
+        received_size = 0
+        f = open(savePath, "wb")
+        while received_size < file_total_size:
+            data = cliSock.recv(BUFSIZ)
+            f.write(data)
+            received_size += len(data)
+            print("已接收：", received_size)
+        f.close()
+        cliSock.send("Receiced ".encode())
 
 
 def usage():
     print("Usage: ")
-    print("  python server.py <path> <ip_addr> <port>")
+    print("  python server.py <folder_path> <ip_addr> <port>")
 
 
 if __name__ == '__main__':
     if(len(sys.argv) < 4):
         usage()
     else:
+        folder_path = sys.argv[1]
         ip_addr = sys.argv[2]
         port = int(sys.argv[3])
+
         serSock = socket(AF_INET, SOCK_STREAM)  # 定义套接字
         serSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         serSock.bind((ip_addr, port))  # 绑定地址
@@ -46,5 +56,6 @@ if __name__ == '__main__':
         print('等待连接...')
         cliSock, addr = serSock.accept()
         print('...连接自:', addr)
-        recv(cliSock)
+
+        recv(cliSock, folder_path)
         serSock.close()
